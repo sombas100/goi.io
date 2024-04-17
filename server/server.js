8e8308571fe11eba659dc20ec9dd1ee1135dbd28
corey clarke <sombas100@yahoo.com>
2024-04-18 00:19:40 +0100

    Connected MongoDB to the server via mongoose

--- server/server.js
+++ server/server.js
@@ -2,7 +2,18 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
+const PORT = 3000
 const app = express();
 
 app.use(express.json());
 app.use(cors())
+
+app.listen(PORT, () => {console.log(`Server is listening on port: ${PORT}`)});
+
+mongoose.connect('mongodb://127.0.0.1:27017/goi')
+.then(() => {
+    console.log('MongoDB connected')
+})
+.catch((error) => {
+    console.log(error)
+})
