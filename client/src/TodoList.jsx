9ed6d23331583c10b8f65f86538d25d49ee14e0d
corey clarke <sombas100@yahoo.com>
2024-04-18 22:21:08 +0100

    Add styling to the app

--- client/src/TodoList.jsx
+++ client/src/TodoList.jsx
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import './index.css'
+import { FaTrash } from "react-icons/fa";
+import { FaEdit } from "react-icons/fa";
 
 const TodoList = () => {
   const [todos, setTodos] = useState([]);
@@ -57,19 +60,19 @@ const TodoList = () => {
   }
 
   return (
-    <div>
-      <h1>Todos</h1>
-      <form onSubmit={handleAddTodo}>
-        <input
+    <div className='todo-list-container'>
+      <h1 className='todo-list-header'>Lets get on it!</h1>
+      <form className='todo-form' onSubmit={handleAddTodo}>
+        <input className='todo-input'
           type="text"
           value={text}
           onChange={(e) => setText(e.target.value)}
           placeholder="Enter task"
           required
         />
-        <button type="submit">Add Todo</button>
+        <button className='todo-submit-btn' type="submit">Add Task</button>
       </form>
-      <ul>
+      <ul className='todo-item'>
         {todos.map(todo => (
             <li key={todo._id}>
                 {editId === todo._id ? (
@@ -79,11 +82,11 @@ const TodoList = () => {
                     <button onClick={() => handleEditTodo(todo._id)}>Save</button>
                     </>
                 ) : (
-                    <>
-                    {todo.text}
-                    <button onClick={() => { setEditId(todo._id); setEditText(todo.text); }}>Edit</button>
-                    <button onClick={() => handleDeleteTodo(todo._id)}>Delete</button>
-                    </>
+                    <div className='edit-buttons'>
+                      {todo.text}
+                      <button className='edit-btn' onClick={() => { setEditId(todo._id); setEditText(todo.text); }}><FaEdit className='icon'/></button>
+                      <button onClick={() => handleDeleteTodo(todo._id)}><FaTrash className='icon'/></button>
+                    </div>
                 )
                 }
             </li>
