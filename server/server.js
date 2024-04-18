b73854124e7340fc93a5e8c2b4bb7cde4431af44
corey clarke <sombas100@yahoo.com>
2024-04-18 01:36:27 +0100

    Mounted routes in the server file and implemented error handling middleware

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const todoRoutes = require('./routes/todoRoutes')
 
 const PORT = 3000
 const app = express();
@@ -17,3 +18,10 @@ mongoose.connect('mongodb://127.0.0.1:27017/goi')
 .catch((error) => {
     console.log(error)
 })
+
+app.use('/api', todoRoutes);
+
+app.use((err, req, res, next => {
+    console.error(err.stack)
+    res.status(err.status || 500).json({ error: 'Internal Server Error' });
+}));
